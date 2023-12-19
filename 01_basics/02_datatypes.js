9a9da5245cc077e99a8ba2b51a0bed22029c4cf6
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-19 19:19:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as a newer version
+
+//alert(3+3) we using nodejs not browser
+//code reliablity should be high
+
+console.log("rohit");
+
+let name = "rohit"
+let age = 23
+let isloggedIN = false
+
+// Data Types (primitive):
+// number => 2 to power 53
+// bigint => use when we need larger numbers
+// string => "" use double cote in JS
+// boolean => true , false
+// null => stand alone vale (object)
+// undefined => ex . let name; this is undefined
+// symbol => when we are talking about unique we are using symbol
+
+// object
+
+console.log(typeof name); // string type
+console.log(typeof age);  // number
+console.log(typeof isloggedIN); //boolean
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
