3a41fd7068c1377dfe9357cd88634b1bb61c21d2
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-20 19:24:15 +0000

    operations

--- 01_basics/03_conversionoprations.js
+++ 01_basics/03_conversionoprations.js
@@ -17,8 +17,8 @@ let result = String(marks)
 
 let booleanNo = 999
 let conversionToBollean = Boolean(booleanNo)
-console.log(typeof conversionToBollean);
-console.log(conversionToBollean);
+//console.log(typeof conversionToBollean);
+//console.log(conversionToBollean);
 
 // 1 => true
 // 0 => false
@@ -26,3 +26,53 @@ console.log(conversionToBollean);
 // "rohit" => true
 // 999 => true
 // "" => false
+
+
+/********************** Operations  ************************************************/
+
+let value = 3
+//console.log(-value); // => -3
+
+//console.log(2+2); => 2
+//console.log(2-3); => -1
+//console.log(2*3); => 6
+//console.log(2**3); => 8
+//console.log(2/3); => 0.6666
+//console.log(2%3); => 2
+
+let str1 = "hi"
+let str2 = " rohit"
+let str3 = str1 + str2
+//console.log(str3);  => hi rohit
+
+//console.log("1" + 2);  => 12
+//console.log(1 + "2");  => 12
+//console.log("1"+2+2);  => 122
+//console.log(1+2+"2");  => 32
+//console.log(1+2+"3"+2+1); => 3321
+
+//console.log(1+2-3*4%8); => -1 (don't do this is not best practice)
+
+//console.log(true); => true
+//console.log(+true); => 1
+//console.log(true+); => error
+//console.log(+""); => 0
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+//console.table([num1 , num2 , num3]) => 4 each but don't do this this is not best practice try in simple way
+
+let gamecounter = 100
+gamecounter++                //postfix
+console.log(gamecounter);    //101
+gamecounter = 200
+++gamecounter                //prefix
+console.log(gamecounter);    //201
+
+
+
+
+
+
+
