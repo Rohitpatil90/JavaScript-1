cc721bc5558220d96aebc5aa6da4f53be093187b
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-20 18:44:10 +0000

    conversion operations

---
+++ 01_basics/03_conversionoprations.js
@@ -0,0 +1,28 @@
+let score = "33"
+//console.log(score);
+//console.log(typeof score);
+
+let marks = Number(score)
+//console.log(typeof marks);
+//console.log(marks);
+
+let result = String(marks)
+//console.log(typeof result);
+//console.log(result);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let booleanNo = 999
+let conversionToBollean = Boolean(booleanNo)
+console.log(typeof conversionToBollean);
+console.log(conversionToBollean);
+
+// 1 => true
+// 0 => false
+// rohit => error
+// "rohit" => true
+// 999 => true
+// "" => false
