9360055adea5d6b9302a14b3999304347b755c15
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-23 17:55:22 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+//Array :
+// 1st the way to declare the array
+const myarray = [0,1,2,3,4,5,5,5]
+const myheros = ["superman", "batman", "spiderman"]
+console.log(myarray);
+console.log(myheros);
+
+// 2nd way to declare array
+const ayarray2 = new Array("suraj", "rahul", "mukesh")
+console.log(ayarray2);
+console.log(ayarray2[2]); // findimg the value using index
+
+// ---------------------------------------------------------
+let myteam = new Array();
+myarray.push("Ruturaj Gaikwad");
+myarray.push("Devon Conway");
+myarray.push("Ajinkya Rahane");
+myarray.push("Darly Mitchell");
+
+console.log(myteam);
+//----------------------------------------------------------------------
+
+let myhome = [1,2,3,4,5]
+let myhome2=myhome.includes(3); //it gives true/false is this value available in array
+console.log(myhome);
+console.log(myhome2);
