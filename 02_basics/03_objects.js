829c8c16143fc40e688135adb48db8a490430ff7
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-24 04:36:52 +0000

    object part 3

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,4 +1,26 @@
 // 03_objects.js
 
 // objects : 
- /*  */
+ /* 
+      
+     singleton = constructor se singleton banega 
+               = literals se single ton nhi banta
+
+    object literals = 
+       const jsuser = {}
+       
+  */
+
+let jsuser = {
+    name : "rohit " ,
+    age : 55 ,
+    div : "A" ,
+    email : "roh1234@gmail.com" ,
+    isloggedIN : true ,
+    lastLoginDays : ["sunday" , "monday" , "wednesday"]
+}
+
+
+console.log(jsuser.email); // 1st way to fetch object property value
+console.log(jsuser["email"]); // 2nd way to fetch property value
+
