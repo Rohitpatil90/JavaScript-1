6c6d07239e98884c14993ad78497c9f01afe6308
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-24 07:02:25 +0000

    object and json

---
+++ 02_basics/05_Object_and_JSON.js
@@ -0,0 +1,35 @@
+// 05_Object_and_JSON.js
+// Object de-structure and json api intro
+
+// de-structure :
+const course = {
+    courseName : "salesforce Dev" ,
+    price : 49999 ,
+    courseInstructor : "rohit" ,
+    coursePeriod : "6 Months"
+}
+
+//course.courseInstructor
+
+const {courseInstructor : ins} = course;  // de-structure
+console.log();
+console.log(ins);
+
+
+//---------------------------------- JSON  ---------------------------------------------
+
+// json in json property write in string and also value in string
+// json format :
+  /*
+   {
+     this is json format 
+   }
+  */
+
+
+   // ex. 
+   {
+    "name" : "js Course"
+    "price" : "999"                       // thid is the syntax of json 
+    "courseInstructor" : "rohit"
+   }
