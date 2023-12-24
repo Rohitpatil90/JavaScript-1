241fa253609fadd65aae3e11987bfa21c0f73a2e
Rohitpatil90 <154263197+Rohitpatil90@users.noreply.github.com>
2023-12-24 06:37:46 +0000

    object part 2

---
+++ 02_basics/04_objects_part2.js
@@ -0,0 +1,13 @@
+// 04_objects_part2.js
+
+// Objects Part 2 :
+   
+/*
+     # singleton : const tinder = new object()   => singleton.
+                 : const tinder= {}              => non-singleton.
+
+                 what is singleton object : singleton object is an object which is declared by using object keyword instead by class.
+                                            no object is required to call methods declared inside singleton object , in scale , 
+                                            there is no static concept. so scala cretes a singleton object to provide entry 
+                                            point your program executon .
+*/
